b36dab04cc5855320eb790f6ca1ae1181b475e4b
Kalpan2007 <kalpankaneriya@gmail.com>
2024-09-29 21:36:27 +0530

    29/09/24

---
+++ js/30.js
@@ -0,0 +1,14 @@
+// 30. Write a program to input an integer 'n' and print the
+// sum of all its even digits and the sum of all its odd digits
+// separately. Example : Input: 'n' = 132456, Output: 12, 9
+function eo(num){
+    if(typeof num !== 'number' ){
+        console.log("this is not number")
+        return;
+    }
+    let even = 0;
+    let odd = 0;
+    let split = num.split('')
+    
+}
+
