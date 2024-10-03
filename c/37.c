40e9d4567e3a6fa268e101c847cd285bade878bf
Kalpan2007 <kalpankaneriya@gmail.com>
2024-10-03 15:22:02 +0530

    38 to 43

--- c/37.c
+++ c/37.c
@@ -1,19 +1,35 @@
-#include <stdio.h>
+//37. Given a sorted array and a target value,
+// return the starting and ending position of
+// that target in the array.Example:Input: [5,
+// 7, 7, 8, 8, 10], target=8 ,Output: [3, 4]
+ #include <stdio.h>
 
 int main(){
 	int arr[]={5,7,7,8,8,10};
-	int ans[]={};
-	int fans[]={};
 	int len1 = sizeof(arr)/sizeof(arr[0]);
+	int first=0;
+	int last=0;
 	int target;
 	printf("enter a targeted value : ");
 	scanf("%d",&target);
 	int i;
 	for(i=0;i<len1;i++){
 		if(arr[i]==target){
-			ans[i]=i;
+			if(first==0){
+			    first = i;
+			}
+			last = i;
 		}
+		
+			
+		
 	}
-	int len2 = sizeof(ans)/sizeof(ans[0]);
-	printf("%d",len2);
+int ans[]={};
+ans[0]=first;
+ans[1]=last;
+int k;
+for(k=0;k<2;k++){
+	printf("%d\n",ans[k]);
+}
+return 0;
 }
